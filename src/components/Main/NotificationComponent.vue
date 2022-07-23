918495a904ee2acb7d95aaec638faf3f5e9a2d4d
pickle99 <jackrestler@gmail.com>
2022-07-23 22:08:29 +0400

    feat: prevent showing duplicate like reactions

--- src/components/Main/NotificationComponent.vue
+++ src/components/Main/NotificationComponent.vue
@@ -39,7 +39,18 @@ export default {
   created(){
     window.Echo.private('showNotification.' + this.userId)
       .listen('ShowNotification', ({notification}) => {
-          this.notifications.unshift(notification);
+        console.log(notification);
+          if(!notification.comment_id){
+            const isQuoteLiked = this.notifications.find((item) => item.user_id === notification.user_id && item.quote_id === notification.quote_id && item.like_id !== null);
+
+            console.log(isQuoteLiked, '123');
+            if(!isQuoteLiked){
+              this.notifications.unshift(notification);
+            }
+          } else {
+            this.notifications.unshift(notification);
+            console.log('here')
+          }
       });
   },
   methods: {
