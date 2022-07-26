11d1f94cddfe0cc09e1e911c964c22f23e95d41b
pickle99 <jackrestler@gmail.com>
2022-07-26 22:13:55 +0400

    style: refactor design for landing page, also add mobile responsive

--- src/components/Landing/LandingComponent.vue
+++ src/components/Landing/LandingComponent.vue
@@ -1,9 +1,11 @@
 <script>
 import SetLanguage from "@/components/Landing/SetLanguage.vue";
 import IconLine from "@/components/icons/IconLine.vue";
+import IconLineSmall from "@/components/icons/IconLineSmall.vue";
 
 export default {
   components: {
+    IconLineSmall,
     IconLine,
     SetLanguage,
   },
@@ -11,29 +13,29 @@ export default {
 </script>
 
 <template>
-  <div class="h-screen overflow-y-scroll overflow-x-hidden snap-y snap-mandatory">
+  <div class="h-screen overflow-y-scroll overflow-x-hidden md:snap-mandatory md:snap-y">
     <div class="snap-start"></div>
-    <header class="flex justify-between mx-40 mt-5 items-center">
+    <header class="flex justify-between mx-3 md:mx-40 mt-5 items-center">
       <div>
         <h1 class="text-[#DDCCAA]">{{ $t("message.movie_quotes") }}</h1>
       </div>
 
       <div class="flex text-white items-center">
-        <SetLanguage />
+        <SetLanguage class="mr-4 md:mr-0"/>
         <RouterLink
-          class="mx-5 bg-red-500 px-6 py-2 rounded-md"
+          class="mx-5 bg-red-500 px-6 py-2 rounded-md hidden md:block"
           :to="{ name: 'register' }"
           >{{ $t("message.sign_up") }}</RouterLink
         >
         <RouterLink
           class="border-white border-2 px-6 py-2 rounded-md"
           :to="{ name: 'login' }"
-          >{{ $t("message.sign_in") }}</RouterLink
+          >{{ $t("message.login") }}</RouterLink
         >
       </div>
     </header>
-    <div class="flex justify-end items-center h-[25rem] flex-col">
-      <div class="mb-10 text-[#DDCCAA] text-6xl text-center">
+    <div class="flex justify-end items-center mt-[10rem] flex-col">
+      <div class="mb-10 text-[#DDCCAA] text-3xl md:text-6xl font-bold text-center">
         <h1>
           {{ $t("message.find_any_quote") }}
           <h1 class="mt-3">{{ $t("message.millions_of_movie_lines") }}</h1>
@@ -46,91 +48,87 @@ export default {
         >{{ $t("message.get_started") }}</RouterLink
       >
     </div>
-    <div class="mt-60 font-bold">
-    <article>
-      <div
-        class="snap-center w-screen h-[75rem] bg-no-repeat bg-cover bg-bottom bg-fixed bg-[url('@/assets/images/movies/Interstellar.png')]">
-        <div class="w-full flex  items-center absolute h-full text-4xl text-white">
+    <article class="flex">
+      <div class="md:mt-64 mt-10 font-bold">
 
-          <div class="ml-16 w-fit h-fit absolute z-10 flex items-center">
+        <div class="">
+          <div class="text-white flex items-center mt-64 md:mt-[45rem]">
+            <div class="w-full md:h-[74rem] h-full absolute bg-no-repeat md:bg-cover bg-contain bg-center md:bg-fixed bg-[url('@/assets/images/movies/Interstellar.png')]" >
+
+            </div>
             <div>
-              <IconLine/>
+              <div class="absolute z-10 ml-3 md:ml-16">
+                <div class="flex snap-center">
+                  <div class="mt-2 mr-3">
+                    <IconLineSmall class="md:hidden"/>
+                    <IconLine class="hidden md:block mt-3"/>
+                  </div>
+                  <div>
+                    <h1 class="mr-36 md:text-5xl md:w-[40rem]">“You have to leave something behind to go forward”</h1>
+                    <p class="mt-3 md:text-2xl">Interstellar, 2014</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="w-fit ml-12 w-[39rem] mt-28">
-              <h1>"You have to leave something behind to go forward"
-              </h1>
-              <p class="mt-10">Interstellar, 2014</p>
+            <div
+              class="bg-gradient-to-r h-[16.5rem] opacity-90 from-black absolute  md:h-[74rem] w-screen"
+            ></div>
+          </div>
+
+          <div class="text-white flex items-center mt-64 md:mt-[74rem] ">
+            <div class="absolute w-full md:h-[74rem] h-full absolute bg-no-repeat md:bg-cover bg-contain bg-center md:bg-fixed bg-[url('@/assets/images/movies/TheRoyalTenenbaums.png')]" >
+
             </div>
+            <div class="absolute z-10 ml-3 md:ml-16">
+              <div class="flex snap-center">
+                <div class="mt-2 mr-3">
+                  <IconLineSmall class="md:hidden"/>
+                  <IconLine class="hidden md:block mt-3"/>
+                </div>
+                <div>
+                  <h1 class="mr-36 md:text-5xl md:w-[40rem]">“I think we’re just gonna
+                    have to be secretly in love
+                    with earch other and
+                    leave it that”</h1>
+                  <p class="mt-3 md:text-2xl">The Royal Tenenbaums,2001</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="bg-gradient-to-r h-[16.3rem] opacity-90 from-black absolute  md:h-[74.1rem] w-screen"
+            ></div>
+          </div>
+
+          <div class="text-white flex items-center mt-64 md:mt-[74rem]">
+            <div class="absolute snap-end" >
+              <img src="@/assets/images/movies/TheRoyalTenenbaums-2.png" alt="img"/>
+            </div>
+            <div class="absolute z-10 ml-3 md:ml-16">
+              <div class="flex snap-center">
+                <div class="mt-2 mr-3">
+                  <IconLineSmall class="md:hidden"/>
+                  <IconLine class="hidden md:block mt-3"/>
+                </div>
+                <div>
+                  <h1 class="mr-36 md:text-5xl md:w-[40rem]">“I see in your eyes the same
+                    fear that would take the
+                    heart of me....”</h1>
+                  <p class="mt-3 md:text-2xl">The Lord of the Rings, 2003</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="bg-gradient-to-r  h-[16.5rem] opacity-90 from-black absolute  md:h-[74.4rem] w-screen"
+            ></div>
           </div>
         </div>
-        <div class="w-screen h-[75rem]">
-          <div
-            class="bg-gradient-to-r opacity-90 from-black  h-[75rem] w-screen"
-          ></div>
-          <div
-            class="bg-gradient-to-b opacity-90 from-black  h-[75rem] w-screen"
-          ></div>
-        </div>
-      </div>
-
-      <div
-        class="w-screen h-[75rem] snap-center bg-no-repeat bg-cover bg-bottom bg-fixed bg-[url('@/assets/images/movies/TheRoyalTenenbaums.png')]">
-        <div class="w-full flex  items-center absolute h-full text-4xl text-white">
-
-          <div class="ml-16 w-fit h-fit absolute z-10 flex items-center">
-            <div>
-              <IconLine/>
-            </div>
-            <div class="w-fit ml-12 w-[38rem] mt-28">
-              <h1>"I think we’re just gonna have
-                to be secretly in love with earch other
-                and leave it that"
-              </h1>
-              <p class="mt-10">The Royal Tenenbaums,2001 </p>
-            </div>
-          </div>
-        </div>
-        <div class="w-screen h-[75rem]">
-          <div
-            class="bg-gradient-to-r opacity-90 from-black  h-[75rem] w-screen"
-          ></div>
-          <div
-            class="bg-gradient-to-b opacity-90 from-black  h-[75rem] w-screen"
-          ></div>
-        </div>
-      </div>
-
-      <div
-        class="w-screen h-[75rem] snap-center bg-no-repeat bg-cover bg-bottom bg-fixed bg-[url('@/assets/images/movies/TheRoyalTenenbaums-2.png')]">
-        <div class="w-full flex  items-center absolute h-full text-4xl text-white">
-
-          <div class="ml-16 w-fit h-fit absolute z-10 flex items-center">
-            <div>
-              <IconLine/>
-            </div>
-            <div class="w-fit ml-12 w-[38rem] mt-28">
-              <h1>"I think we’re just gonna have
-                to be secretly in love with earch other
-                and leave it that"
-              </h1>
-              <p class="mt-10">The Royal Tenenbaums,2001 </p>
-            </div>
-          </div>
-        </div>
-        <div class="w-screen h-[75rem]">
-          <div
-            class="bg-gradient-to-r opacity-90 from-black  h-[75rem] w-screen"
-          ></div>
-        </div>
+        <footer class="snap-end text-[#DDCCAA]  ml-3 md:ml-0 text-sm md:text-xl bg-[#0D0B14] mt-32 md:mt-[37rem]">
+          <p class="md:ml-32 py-5">
+            © 2022 {{ $t("message.movie_quotes") }}.
+            {{ $t("message.all_rights_reserved") }}
+          </p>
+        </footer>
       </div>
     </article>
-
-    </div>
-    <footer class="snap-end text-[#DDCCAA] bg-[#0D0B14] absolute top-[269rem]">
-      <p class="ml-32 py-5">
-        © 2022 {{ $t("message.movie_quotes") }}.
-        {{ $t("message.all_rights_reserved") }}
-      </p>
-    </footer>
   </div>
 </template>
---
+++ src/components/icons/IconLineSmall.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="17" height="2" viewBox="0 0 17 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <line x1="-8.74228e-08" y1="1" x2="17" y2="0.999999" stroke="white" stroke-width="2"/>
+  </svg>
+
+</template>
